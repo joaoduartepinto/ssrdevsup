b8e82d40a0603825c76b79c5ee399d912529b2ee
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-05-03 19:39:46 +0100

    Project created

---
+++ backend/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+u
+Esrc/main/java/com/joaoduartepinto/desvendas/DesvendasApplication.java,4/e/4ec4e711dfe2555fab02baad6cdcf63a557b4ea1
---
+++ backend/src/test/java/com/joaoduartepinto/desvendas/DesvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoduartepinto.desvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
