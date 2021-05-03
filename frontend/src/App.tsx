a8de42a8b3ba67c932acc570ead768c046be6ae1
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-05-03 22:26:35 +0100

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,37 @@ import React from 'react';
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import DataTable from "./components/DataTable";
+import BarChart from "./components/BarChart";
+import DonutChart from "./components/DonutChart";
 
 function App() {
     return (
         <>
             <NavBar/>
             <div className="container">
-                <h1 className="text-primary">Hey!</h1>
+                <h1 className="text-primary">Sales</h1>
+
+                <div className="row px-3">
+
+                    <div className="col-sm-6">
+                        <h4 className="text-center text-secondary">Percentage of Sales</h4>
+                        <BarChart/>
+                    </div>
+
+                    <div className="col-sm-6">
+                        <h4 className="text-center text-secondary">Total Sales</h4>
+                        <DonutChart/>
+                    </div>
+
+                </div>
+
+                <div className="py-3">
+                    <h2 className="text-primary">
+                        Total Sales
+                    </h2>
+                </div>
+
+
                 <DataTable/>
             </div>
             <Footer/>
