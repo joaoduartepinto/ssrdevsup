9e22b4cb0c14f98810a23861d0806362a1b53829
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-05-03 21:49:34 +0100

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import DataTable from "./components/DataTable";
 
 function App() {
-  return (
-    <div>
-      <h1 className={"text-primary"}>Hey!</h1>
-    </div>
-  );
+    return (
+        <>
+            <NavBar/>
+            <div className="container">
+                <h1 className="text-primary">Hey!</h1>
+                <DataTable/>
+            </div>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
