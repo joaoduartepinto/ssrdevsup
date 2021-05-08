50cf2b0314c0edf79d4e9663ff64708ab53a64ea
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-05-08 15:35:20 +0100

    Fixed bug in pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -12,13 +12,13 @@ const Pagination = ( {page, onPageChange} : Props ) => {
         <div className="row d-flex justify-content-center">
             <nav>
                 <ul className="pagination">
-                    <li className={`page-item ${page.first ? 'disabled' : ''} `}>
+                    <li className={`page-item ${(page.first) ? 'disabled' : ''} `}>
                         <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Previous</button>
                     </li>
                     <li className="page-item disabled">
                         <span className="page-link">{page.number + 1}</span>
                     </li>
-                    <li className={`page-item ${page.last ? 'disabled' : ''} `}>
+                    <li className={`page-item ${(page.last) ? 'disabled' : ''} `}>
                         <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Next</button>
                     </li>
                 </ul>
